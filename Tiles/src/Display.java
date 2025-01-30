c622500d024a533041903c5b1d01aef11ac928e0
Youssef Amin <yamin@unm.edu>
2025-01-30 04:59:19 -0700

    I should not have done the GUI first

--- Tiles/src/Display.java
+++ Tiles/src/Display.java
@@ -1,4 +1,3 @@
-import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.*;
@@ -16,8 +15,28 @@ public class Display {
     private final int SCREEN_HEIGHT = 500;
     private final int SCREEN_WIDTH = 500;
     private final int NUM_ROWS = 5;
-    private final int NUM_COLS = 6;4
-    
+    private final int NUM_COLS = 6;
+
+    private final Map<Color, Integer> colors = new HashMap<>();
+
+    public Display() {
+        //colors taken from the internet
+        colors.put(Color.web("#E57373"), 0); // Rose
+        colors.put(Color.web("#81C784"), 0); // Green
+        colors.put(Color.web("#64B5F6"), 0); // Blue
+        colors.put(Color.web("#BA68C8"), 0); // Purple
+        colors.put(Color.web("#F28C28"), 0); // Distinct Orange
+        colors.put(Color.web("#4DB6AC"), 0); // Teal
+        colors.put(Color.web("#9575CD"), 0); // Deep Purple
+        colors.put(Color.web("#A1887F"), 0); // Brown
+        colors.put(Color.web("#F06292"), 0); // Pink
+        colors.put(Color.web("#7986CB"), 0); // Indigo
+        colors.put(Color.web("#D32F2F"), 0); // Vibrant Red (instead of duplicate orange)
+        colors.put(Color.web("#AED581"), 0); // Light Green
+        colors.put(Color.web("#00838F"), 0); // Dark Cyan
+        colors.put(Color.web("#546E7A"), 0); // Steel Grey
+        colors.put(Color.web("#007FFF"), 0); // Electric Blue
+    }
     public void setStage(Stage stage) {
         stage.setTitle("Tiles");
         StackPane root = new StackPane();
@@ -29,12 +48,6 @@ public class Display {
     }
 
     private GridPane makeGrid() {
-        Map<Color, Integer> colors = new HashMap<>();
-        colors.put(Color.web("#E57373"), 0);
-        colors.put(Color.web("#81C784"), 0);
-        colors.put(Color.web("#64B5F6"), 0);
-        colors.put(Color.web("#BA68C8"), 0);
-        colors.put(Color.web("#FF8A65"), 0);
 
 
         GridPane gp = new GridPane();
@@ -48,11 +61,11 @@ public class Display {
         for (int row = 0; row < NUM_ROWS; row++) {
             for (int col = 0; col < NUM_COLS; col++) {
 
-                //TODO: Use getColors here
-                Color sqColor;
-                Color cirColor;
-                Color triColor;
-                colorIndex++;
+
+                List<Color> colors = getColors();
+                Color sqColor = colors.get(0);
+                Color cirColor = colors.get(1);
+                Color triColor = colors.get(2);
 
                 int n = row * NUM_COLS + col;
 
@@ -79,8 +92,31 @@ public class Display {
         return gp;
     }
 
-    private Color getColors(Map<Color, Integer> colors){
-        //TODO: Randomly choose colors whos count is < 6
-        return null;
+    private List<Color> getColors(){
+        List<Color> newColors = new ArrayList<>();
+
+        for (Map.Entry<Color, Integer> entry : colors.entrySet()) {
+            if (entry.getValue() < 18) {
+                newColors.add(entry.getKey());
+            }
+        }
+
+
+        Collections.shuffle(newColors);
+
+        if (newColors.size() < 3) {
+            System.out.println("Not enough colors available!");
+            return Arrays.asList(null, null, null);
+        }
+
+        Color sqColor = newColors.get(0);
+        Color cirColor = newColors.get(1);
+        Color triColor = newColors.get(2);
+
+        colors.put(sqColor, colors.get(sqColor) + 1);
+        colors.put(cirColor, colors.get(cirColor) + 1);
+        colors.put(triColor, colors.get(triColor) + 1);
+
+        return Arrays.asList(sqColor, cirColor, triColor);
     }
-}
+    }
